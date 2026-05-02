b045e05bf12b2aff46edabeef6f6f293e1292e13
COLANT Alan <alan.colant@uni-deal.com>
2026-05-02 22:35:34 +0200

    switch to prehype-mermaid

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,13 +1,19 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
-import mermaid from 'astro-mermaid';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://epsicube.dev',
+    markdown: {
+        syntaxHighlight: {
+            type: 'shiki',
+            excludeLangs: ['mermaid', 'math'],
+        },
+        rehypePlugins: [[rehypeMermaid, { strategy: "img-svg", dark: true, colorScheme: "forest" }]]
+    },
     integrations: [
-        mermaid({}),
         starlight({
             title: 'Epsicube Framework',
             description: 'Modular framework built on Laravel.',
